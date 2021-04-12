d82a4023f1704f8bd13724ccbf6d9924c865d35d
Cuichanghe <1337985150@qq.com>
2021-04-12 22:28:09 +0800

    V0.4

--- sh/getlist.sh
+++ sh/getlist.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 dir=$(ls -l /home/ |awk '/^d/ {print $NF}')
+if [ -e "/usr/CGcode-server/list/cguserlist" ]; then
+	rm -f /usr/CGcode-server/list/cguserlist
+	touch /usr/CGcode-server/list/cguserlist
+else
+	touch /usr/CGcode-server/list/cguserlist
+fi
 for i in $dir
 do
 if [ -e "/home/${i}/.isfcg" ]; then
-echo ${i} >> /usr/CGcode-server/list/cguserlist;
+	echo ${i} >> /usr/CGcode-server/list/cguserlist;
 else
 	echo "用户： ${i} 不是CGcode-server用户！"
 fi
